901fea6506cf9ee4f70e9d7b2dcf25cdbade8320
shone123 <sujaytalukder03@gmail.com>
2022-03-16 16:56:08 +0530

    Form validaiton and API integration is done

--- scripts/app.js
+++ scripts/app.js
@@ -1,48 +1,77 @@
-var app = angular.module("addrBook",[]);
-    app.controller("contactListCtrl",'myhttpService', function ($scope,  myhttpService) {
 
-        $scope.info = null;
-        myhttpService.httpGetService().then(function(response){
-            $scope.info = response.data;
-         }, function(error){
-            console.log(error);
-         });
+
+var app = angular.module('addrBook',['contactData']);
+    app.controller("contactListCtrl", function ($scope, MyhttpService) {
+
+    $scope.info = null;
+
+    $scope.newFirstName="";
+    $scope.newLastName="";
+    $scope.newPhone="";
+
+    MyhttpService.httpGetService().then(function(response){
+        $scope.info = response.data;
+        }, function(error){
+        console.log(error);
+    });
+    
         
-        $scope.addMe = function(){
-            return {
-                firstName: $scope.newFirstName,
-                lastName:$scope.newLastName,
-                phoneNumber: $scope.newPhone
+    app.directive('numbersOnly', function () {
+        return {
+            require: 'ngModel',
+            link: function (scope, element, attr, ngModelCtrl) {
+                function fromUser(text) {
+                    if (text) {
+                        var transformedInput = text.replace(/[^0-9]/g, '');
+
+                        if (transformedInput !== text) {
+                            ngModelCtrl.$setViewValue(transformedInput);
+                            ngModelCtrl.$render();
+                        }
+                        return transformedInput;
+                    }
+                    return undefined;
+                }            
+                ngModelCtrl.$parsers.push(fromUser);
             }
+        };
+    });
+
+    $scope.filterValue = function($event){
+        if(isNaN(String.fromCharCode($event.keyCode))){
+            $event.preventDefault();
         }
-        $scope.addItem = function(){
-            for(var i=0;i<$scope.info.length;i++){
-                if(($scope.info[i].name == $scope.addMe().name)||
-                ($scope.info[i].tel == $scope.addMe().tel)){
-                    alert("name or phone number was repeated");
-                    return false;
-                }
-            }
+    };
+    
+    $scope.addMe = function(){
+        return {
+            firstName: $scope.newFirstName,
+            lastName:$scope.newLastName,
+            phone: $scope.newPhone
+        }
+    }
+    $scope.addItem = function(){
+        if($scope.contactForm.$invalid) return false;
             $scope.info.push($scope.addMe());
+    }
+        
+    $scope.removeItem = function(){
+        $scope.info.splice(this.$index,1)
+    }      
+    $scope.change = function(){
+        index = this.$index;
+        $scope.showMe = function(indx){
+            if(indx == index){
+            return true;                 
             }
-            
-        $scope.removeItem = function(){
-            $scope.info.splice(this.$index,1)
-        }      
-        $scope.change = function(){
-            index = this.$index;
-            $scope.showMe = function(indx){
-                if(indx == index){
-                return true;                 
-                }
-            }    
-        }
-        $scope.save = function(){
-            index = this.$index;
-            $scope.showMe = function(indx){
-                if(indx == index){
-                return false;                 
-                }
-            }    
-        }            
-    })
+        }    
+    }
+    $scope.save = function(){
+        index = this.$index;
+        $scope.showMe = function(indx){
+            if(indx == index){
+            return false;                 
+            }
+        }    
+    }            
+})
--- scripts/services/contactlist-data-service.js
+++ scripts/services/contactlist-data-service.js
@@ -1,6 +1,6 @@
 
-var app = angular.module("addrBook",[]);
-app.service("myhttpService",['$http', function ($http) {
+var app = angular.module("contactData",[]);
+app.service("MyhttpService",['$http', function ($http) {
     this.httpGetService = function () {
        return $http.get('https://my-json-server.typicode.com/voramahavir/contacts-mock-response/contacts');
     }
